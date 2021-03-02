73ea36de6346ea89a7a66560b739d66b4b1611e6
Joel <joel.d.hart1@gmail.com>
2021-03-02 17:27:50 +0000

    updated with correct slice array function to remove first item of array

--- tail.js
+++ tail.js
@@ -6,8 +6,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 const tail = function(ary){
-  return ary.slice(0)
+  return ary.slice(1)
 }
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
