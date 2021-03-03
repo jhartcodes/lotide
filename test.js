f72616a3fc486cffbc1d4031ad0c461890131664
Joel <joel.d.hart1@gmail.com>
2021-03-03 05:18:19 +0000

    compare 2 lists to see if they have the exact same value

---
+++ test.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:"${actual} !==  ${expected}`);
+  }
+};
+
+const eqArrays = function(listOne, listTwo) {
+    if(listOne.length === listTwo.length) {
+      for (let i = 0; i <listOne.length; i++) {
+        if (listOne[i] !== listTwo[i]) {
+          return false;
+      }
+    } 
+    return true 
+  } else {
+    return false; 
+  }
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
