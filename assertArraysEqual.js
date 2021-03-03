7b305ecdc08b3681e1b49b343f3b03a94b7bb6be
Joel <joel.d.hart1@gmail.com>
2021-03-03 06:37:17 +0000

    function using assertEquals function to return false or true statement when comparing arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+
+
+const eqArrays = function(listOne, listTwo) {
+  if (listOne.length === listTwo.length) {
+    for (let i = 0; i < listOne.length; i++) {
+      if (listOne[i] !== listTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} != ${arrTwo}`);
+  }
+};
+assertArraysEqual([1,2,3],[1,2,3]);
