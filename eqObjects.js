90c4f907a08fa6099f14b6041e19e3a09c51c5d8
Joel <joel.d.hart1@gmail.com>
2021-03-07 21:54:33 +0000

    eqobjects final update

--- eqObjects.js
+++ eqObjects.js
@@ -6,24 +6,48 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqObjects = function (object1, object2) {
-
-
-  if (Object.keys(object1.length) !== Object.keys(object2.length)) {
-    return false
-  }
-  for (const letter in object1) {
-    if (object1[letter] === ob) {
-
+const eqArrays = function (listOne, listTwo) {
+  if (listOne.length === listTwo.length) {
+    for (let i = 0; i < listOne.length; i++) {
+      if (listOne[i] !== listTwo[i]) {
+        return false;
+      }
     }
+    return true;
+  } else {
+    return false;
   }
 };
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
 
-assertEqual(ab, abc),); // => false
-assertEqual(ab, ba); // => true
+const eqObjects = function (object1, object2) {
+  if ((Object.keys(object1)).length !== (Object.keys(object2)).length) {
+    return false
+  }
+  for (key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+      eqArrays(object1, object2)
+    }
+    else if (object1[key] !== object2[key]) {
+      return false
+    }
+  } return true
+}
 
-// could this part of the code be wrong im stuck on the first step//
-console.log(Object.keys(object1.length))
+
+
+assertEqual(eqObjects(cd, dc), true); // => true
+
+assertEqual(eqObjects(cd, cd2), false); // => false
+
+// assertEqual(eqObjects(ab, ba), true); // => true
+// assertEqual(eqObjects(ab, abc), false); // => false
+
+
+
