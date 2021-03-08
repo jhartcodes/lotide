f4030f46098a166992b5fa286c963250fc100ac9
Joel <joel.d.hart1@gmail.com>
2021-03-08 22:47:26 +0000

    refactor middle and added test file that include assertarraysequal

--- middle.js
+++ middle.js
@@ -1,40 +1,14 @@
-const eqArrays = function(listOne, listTwo) {
-  if (listOne.length === listTwo.length) {
-    for (let i = 0; i < listOne.length; i++) {
-      if (listOne[i] !== listTwo[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(arrOne, arrTwo) {
-  if (eqArrays(arrOne, arrTwo)) {
-    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} != ${arrTwo}`);
-  }
-};
-
-const middle = function(arr) {
+const middle = function (arr) {
   let newArray = [];
   if (arr.length < 3) {
     return newArray;
   } else if (arr.length % 2 === 1) {
     newArray.push(arr[(arr.length / 2) - .5]);
   } else if (arr.length % 2 === 0) {
-    newArray.push(arr[(arr.length / 2)- 1]);
+    newArray.push(arr[(arr.length / 2) - 1]);
     newArray.push(arr[(arr.length / 2)]);
   }
   return newArray;
 };
 
-assertArraysEqual(middle([1]),[])
-assertArraysEqual(middle([1, 2]),[])
-assertArraysEqual(middle([1, 2, 3]),[2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4])
+module.exports = middle
