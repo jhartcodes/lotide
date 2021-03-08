1e633c4dfcab1ac88be8e1479d6c73424ac3f62e
Joel <joel.d.hart1@gmail.com>
2021-03-08 21:47:21 +0000

    adjusted head function to have a test file and put the call functions in the test

--- head.js
+++ head.js
@@ -2,8 +2,7 @@ const assertEqual = require('./assertEqual');
 
 const head = function (ary) {
   return ary[0]
-}
+};
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head
 
