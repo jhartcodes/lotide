17cce27bc7c3380b1ae08008d201fcfad1126695
Joel <joel.d.hart1@gmail.com>
2021-03-08 21:26:30 +0000

    added test file for assertEqual and removed the test variables to be put in test file

--- head.js
+++ head.js
@@ -1,14 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:"${actual} !==  ${expected}`);
-  }
-};
-const head = function(ary){
+const assertEqual = require('./assertEqual');
+
+const head = function (ary) {
   return ary[0]
 }
 
-
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
