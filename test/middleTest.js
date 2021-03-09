abb1b111840d6d17d6e6e28e4607d90902a9624e
Joel <joel.d.hart1@gmail.com>
2021-03-09 00:49:44 +0000

    refactored middle and tail tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,18 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle')
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns the middle integer of the string", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  });
+});
 
 
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+
+  // assertArraysEqual(middle([1]), [])
+  // assertArraysEqual(middle([1, 2]), [])
+  // assertArraysEqual(middle([1, 2, 3]), [2])
+  // assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+  // assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
+  // assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,11 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail')
+const assert = require('chai').assert;
 
+describe("tail function", () => {
+  it("returns 2 for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.strictEqual(tail(words), 2);
+  });
+});
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
